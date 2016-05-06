d8d142c6128f29fc8d8a8a27d800f958dd26ef9a
metereol <skistner@mica.edu>
2016-05-05 21:12:28 -0400

    ww

--- app/sketch.js
+++ app/sketch.js
@@ -70,6 +70,16 @@ var stepsize = .0001;
 var humidBuffer;
 
 
+window.onload = function() {
+  var startPos;
+  var geoSuccess = function(position) {
+    startPos = position;
+    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+  };
+  navigator.geolocation.getCurrentPosition(geoSuccess);
+};
+
 function preload() {
   calibre = loadFont('assets/Calibre-Black.otf');
   url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zip + '&units=imperial&APPID=dbc1eb3f8bcd39cf9ae676b83e2e514c';
