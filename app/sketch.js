270c876fc791e6b9461228a35c8e9da463a396dd
metereol <skistner@mica.edu>
2016-05-06 00:55:11 -0400

    removed

--- app/sketch.js
+++ app/sketch.js
@@ -77,15 +77,6 @@ var stepsize = .0001;
 var humidBuffer;
 
 
-window.onload = function() {
-  var startPos;
-  var geoSuccess = function(position) {
-    startPos = position;
-    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-  };
-  navigator.geolocation.getCurrentPosition(geoSuccess);
-};
 
 function preload() {
   calibre = loadFont('assets/Calibre-Black.otf');
